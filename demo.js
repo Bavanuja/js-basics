3191e756ed90e12ed9603e54eb438fc2966cdc67
Bavanuja <bbavanuja@gmail.com>
2024-07-11 15:32:39 +0530

    for

--- demo.js
+++ demo.js
@@ -1,12 +1,4 @@
-var season="spring"
-if (season =="spring"){
-    console.log("enjoy")
-}
-else if (season=="summer"){
-    console.log("have fun")
-
-}
-else{
-    console.log("so sad")
+for(b=1;b<=10;b+=2){
+    console.log("banu")
 
 }
